02ba1aba125ea8ccea0e9bad1a6e9d7ba3284c6d
SirBenWard <sirbenward@users.noreply.github.com>
2019-01-17 14:03:57 -0800

    Added singleRun.

--- src/main/java/com/cloudcore/grader/Main.java
+++ src/main/java/com/cloudcore/grader/Main.java
@@ -12,6 +12,7 @@ public class Main {
 
     public static void main(String[] args) {
         SimpleLogger.writeLog("ServantStarted", "");
+        singleRun = isSingleRun(args);
         if (args.length != 0 && Files.exists(Paths.get(args[0]))) {
             System.out.println("New root path: " + args[0]);
             FileSystem.changeRootPath(args[0]);
@@ -20,18 +21,34 @@ public class Main {
 
         FolderWatcher watcher = new FolderWatcher(FileSystem.DetectedFolder);
 
-        if (0 != FileSystem.getTotalCoins(FileSystem.DetectedFolder)[5])
+        if (0 != FileSystem.getTotalCoins(FileSystem.DetectedFolder)[5]) {
             Grader.grade();
+            exitIfSingleRun();
+        }
 
         while (true) {
             try {
                 Thread.sleep(1000);
 
-                if (watcher.newFileDetected())
+                if (watcher.newFileDetected()) {
                     Grader.grade();
+                    exitIfSingleRun();
+                }
             } catch (Exception e) {
                 System.out.println("Uncaught exception - " + e.getLocalizedMessage());
             }
         }
+    }
+
+    public static boolean singleRun = false;
+    public static boolean isSingleRun(String[] args) {
+        for (String arg : args)
+            if (arg.equals("singleRun"))
+                return true;
+        return false;
+    }
+    public static void exitIfSingleRun() {
+        if (singleRun)
+            System.exit(0);
     }
 }
