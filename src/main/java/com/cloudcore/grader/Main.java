69ed457ecb84ca44760f691e803f9d9d1fec3f5f
SirBenWard <sirbenward@users.noreply.github.com>
2019-01-17 13:59:29 -0800

    Added timer.

--- src/main/java/com/cloudcore/grader/Main.java
+++ src/main/java/com/cloudcore/grader/Main.java
@@ -6,32 +6,29 @@ import com.cloudcore.grader.utils.SimpleLogger;
 
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.time.Instant;
 
 public class Main {
 
 
     public static void main(String[] args) {
         SimpleLogger.writeLog("ServantStarted", "");
+        if (args.length != 0 && Files.exists(Paths.get(args[0]))) {
+            System.out.println("New root path: " + args[0]);
+            FileSystem.changeRootPath(args[0]);
+        }
+        FileSystem.createDirectories();
+
+        FolderWatcher watcher = new FolderWatcher(FileSystem.DetectedFolder);
+
+        if (0 != FileSystem.getTotalCoins(FileSystem.DetectedFolder)[5])
+            Grader.grade();
 
         while (true) {
             try {
-                if (args.length != 0 && Files.exists(Paths.get(args[0]))) {
-                    System.out.println("New root path: " + args[0]);
-                    FileSystem.changeRootPath(args[0]);
-                }
-                FileSystem.createDirectories();
+                Thread.sleep(1000);
 
-                FolderWatcher watcher = new FolderWatcher(FileSystem.DetectedFolder);
-                boolean stop = false;
-
-                if (0 != FileSystem.getTotalCoins(FileSystem.DetectedFolder)[5])
+                if (watcher.newFileDetected())
                     Grader.grade();
-
-                while (!stop) {
-                    if (watcher.newFileDetected())
-                        Grader.grade();
-                }
             } catch (Exception e) {
                 System.out.println("Uncaught exception - " + e.getLocalizedMessage());
             }
